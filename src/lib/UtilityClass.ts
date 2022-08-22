781965b8fb1ce3dd11f2890d88da94662cc8ec65
Krzysztof Siuda <kksiuda@gmail.com>
2022-08-22 16:45:19 +0100

    bundle status changes

--- src/lib/UtilityClass.ts
+++ src/lib/UtilityClass.ts
@@ -5,19 +5,43 @@ type Unsubscribe = () => any;
 type CancelTimeout = () => any;
 
 export abstract class UtilityClass<S> {
+  constructor() {
+    this.emitStatusChange = this.emitStatusChange.bind(this);
+  }
   protected emitter = new EventEmitter();
   protected abstract status: S;
+  protected emitScheduleHandle: NodeJS.Immediate | null = null;
+  protected timeouts: { handle: NodeJS.Timeout; cb: () => any; callOnClearAll: boolean }[] = [];
+
   public onStatus(listener: (status: S) => any): Unsubscribe {
     this.emitter.on('status-change', listener);
     listener(this.status);
     return () => this.emitter.off('status-change', listener);
   }
-  protected changeStatus(status: Partial<S>) {
-    const newStatus = merge(this.status, status);
-    this.emitter.emit('status-change', newStatus);
-    this.status = newStatus;
+
+  protected emitStatusChange() {
+    this.emitter.emit('status-change', this.status);
+    this.emitScheduleHandle = null;
   }
-  private timeouts: { handle: NodeJS.Timeout; cb: () => any; callOnClearAll: boolean }[] = [];
+
+  protected scheduleEmit() {
+    if (this.emitScheduleHandle) return;
+    this.emitScheduleHandle = setImmediate(this.emitStatusChange);
+  }
+
+  protected changeStatus(status: Partial<S>) {
+    let statusHasChanged = false;
+    for (const key of Object.keys(status) as (keyof S)[]) {
+      if (status[key] !== this.status[key]) {
+        statusHasChanged = true;
+        break;
+      }
+    }
+    if (!statusHasChanged) return;
+    this.status = merge(this.status, status);
+    this.scheduleEmit();
+  }
+
   protected setTimeout(cb: () => any, ms: number, callOnClearAll = true): CancelTimeout {
     const handle = setTimeout(cb, ms);
     const timeoutCallback = { handle, cb, callOnClearAll };
@@ -28,7 +52,12 @@ export abstract class UtilityClass<S> {
       clearTimeout(handle);
     };
   }
-  protected clearAllTimeouts() {
+
+  public clearAllTimeouts() {
+    if (this.emitScheduleHandle) {
+      clearImmediate(this.emitScheduleHandle);
+      this.emitStatusChange();
+    }
     this.timeouts.forEach(({ handle, cb, callOnClearAll }) => {
       clearTimeout(handle);
       if (callOnClearAll) cb();
